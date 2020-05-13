d272a524a635174de1108c2d6829019c79d5e2a8
Jim Yoon <jimyoon90@gmail.com>
2020-05-13 16:12:21 -0700

    gitignore test.js and finished palindromeNumber

--- easy/palindromeNumber.js
+++ easy/palindromeNumber.js
@@ -1,7 +1,22 @@
+// 9. Palindrome Number
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
 /**
  * @param {number} number
  * @return {boolean}
  */
+
 const palindromeNumber = (number) => {
-    
+  // check if number is a negative
+  if (number < 0) return false;
+  // turn number into a string first
+  const numToString = String(number).split('');
+  // save value before reversing
+  const numToStringValue = numToString.join('');
+  // reverse numToString
+  const reverseNumToString = numToString.reverse().join('');
+  // check if the two are similar, if so return true, else false
+  return numToStringValue === reverseNumToString;
 };
+
+// Follow up: Coud you solve it without converting the integer to a string?
